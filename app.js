14afff84add3fd1c1c8486d2a7692e736a1d144c
bcdguz <bcdguz96@gmail.com>
2021-02-20 17:15:46 -0800

    Creating post routes for login and register

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
+//parse JSON we send to frontend
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //routes
 app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/users", users);
@@ -18,7 +21,3 @@ app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-//parse JSON we send to frontend
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
