707e73e5395566daba639a8798335f7d168e601e
bcdguz <bcdguz96@gmail.com>
2021-02-21 12:07:57 -0800

    Finishing touches on MERN twitter

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -7,7 +7,7 @@ class MainPage extends React.Component {
             <div>
                 <h1>A Twitter Clone</h1>
                 <footer>
-                    Copyright &copy; 2019 Chirper
+                    Copyright &copy; 2019 Chirper -MERN practice by Bryce
         </footer>
             </div>
         );
