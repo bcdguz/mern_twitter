630950fa1743a54551c491b7bd08aef4e83c4700
bcdguz <bcdguz96@gmail.com>
2021-02-20 17:48:01 -0800

    Creating validations for register and login

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 const router = express.Router();
 
