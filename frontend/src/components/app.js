ec66a0fac2313b29f35d4c1912a55484ba6566df
bcdguz <bcdguz96@gmail.com>
2021-02-21 11:02:00 -0800

    Creating signup and login components and containers

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,7 +4,7 @@ import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
 import MainPage from './main/main_page';
-import LoginFormContainer from './session/login_form_container';
+import LoginFormContainer from './session/login_form_container.js';
 import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
